2f97154d07e06e951c7ad127bbfc71e7c5260757
mikuni <39937720+7174952@users.noreply.github.com>
2025-03-18 11:59:00 +0900

    update make up parameters, update launch file

---
+++ src/amr_ros/scripts/init_robot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#setup usb-rs485 adapter modbus cable
+sudo -S su << EOF
+mikuni
+EOF
+sudo su -c 'modprobe ftdi_sio vendor=0x06ce product=0x8331'
+sudo su -c 'echo 06ce 8331 > /sys/bus/usb-serial/drivers/ftdi_sio/new_id'
+
+sudo chmod 777 /dev/ttyUSB*
+
+
